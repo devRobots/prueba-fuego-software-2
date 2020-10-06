67cf393aa01093ff3d2919014ef650767ee8c0cc
tatiana <tatianamora1902@gmail.com>
2020-10-06 17:16:50 -0500

    Login
    se verifica que la contraseña tenga mas de 6 digitos para poder ser verificada, el boton tiene funcionalidad y en una variable llamada account se esta guardando el usuario y contraseña ingresados

--- src/pages/Login/Components/Input/Input.jsx
+++ src/pages/Login/Components/Input/Input.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Input.css';
 
-const Input = () => {
+const Input = ({attribute,handleChange,param}) => {
     return(
         <div>
-            <input />
+            <input
+             id={attribute.id}
+             name={attribute.name}
+             placeholder={attribute.placeholder}
+             type={attribute.type}
+             onChange={ (e) => handleChange(e.target.name,e.target.value)}
+             className= {param ? 'input-error' : 'regular-style'}
+              />
         </div>
     )
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {userState, useState} from 'react';
 import './Login.css';
 import Title from './Components/Title/Title';
 import Label from './Components/Label/Label';
@@ -6,13 +6,66 @@ import Input from './Components/Input/Input';
 
 
 const Login = () => {
+
+    const [user, setUser]= useState('');
+    const [password, setPassword]= useState('');
+    const [passwordError, setPasswordError]= useState(false);
+
+    function handleChange(name,value) {
+        if(name === 'usuario'){
+            setUser(value)
+        }
+        else{
+            if(value.length < 6)
+            {
+                setPasswordError(true);
+            }
+            else{
+                setPasswordError(false);
+                setPassword(value)
+            }
+        }
+    };
+
+    function handleSubmit(params) {
+        let account= {user,password}
+
+        if(account)
+        {
+            console.log('account:', account)
+        }
+    };
     return(
         <div className= 'login-container'>
             <Title text= 'Registrarse'/>
             <Label text= 'Usuario'/>
-            <Input />
+            <Input 
+             attribute={{
+                 id: 'usuario',
+                 name: 'usuario',
+                 placeholder: 'Ingrese su usuario',
+                 type: 'text'  
+             }}
+             handleChange={handleChange}
+             />
             <Label text= 'Contraseña'/>
-            <Input />
+            <Input 
+             attribute={{
+                 id: 'contraseña',
+                 name: 'contraseña',
+                 placeholder: 'Ingrese su contraseña',
+                 type: 'password'  
+             }}
+             handleChange={handleChange}
+             param={passwordError}
+             />
+             { passwordError &&
+             <label className='label-error'>
+                 contraseña invalida o incompleta
+             </label>}
+             <button onClick={handleSubmit}>
+                 Registrarse
+             </button>
         </div>
     )
 }

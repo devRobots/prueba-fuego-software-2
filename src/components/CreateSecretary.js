2ec8062d817304acd501d210127dbe635ac6da4e
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-11-22 18:03:56 -0500

    Mejoras en la interfaz de registro de secretarios

--- src/components/CreateSecretary.js
+++ src/components/CreateSecretary.js
@@ -1,66 +1,66 @@
 import React, { useState } from 'react';
 import './CreateSecretary.css';
-import {Segment, Header, Label, Input, Button} from 'semantic-ui-react';
+import { Segment, Header, Label, Input, Button, Grid, Placeholder } from 'semantic-ui-react';
 import Firebase from '../database/firebase';
 
 import { Redirect } from "react-router-dom";
 
 const CreateSecretary = () => {
 
-    const [celular, setPhone ] = useState('');
+    const [celular, setPhone] = useState('');
     const [password, setPassword] = useState('');
     const [passwordError, setPasswordError] = useState(false);
-    const [ email, setEmail ] = useState('');
-    const [ estado, setEstado ] = useState('');
-    const [ id, setId ] = useState('');
-    const [ nombre, setNombre ] = useState('');
-    const [ usuario, setUser ] = useState('');
+    const [email, setEmail] = useState('');
+    const [estado, setEstado] = useState('');
+    const [id, setId] = useState('');
+    const [nombre, setNombre] = useState('');
+    const [usuario, setUser] = useState('');
 
-    const[isCreateSecretary, setCreateSecretary]= useState(true); 
+    const [isCreateSecretary, setCreateSecretary] = useState(true);
 
-    if(!isCreateSecretary){
-        return <Redirect to = "/login"/>
+    if (!isCreateSecretary) {
+        return <Redirect to="/login" />
     }
 
     function handleChange(name, value) {
-        switch(name) {
+        switch (name) {
             case 'celular':
-                if(value < 1) {
+                if (value < 1) {
 
                 } else {
                     setPhone(value)
                 }
                 break;
             case 'usuario':
-                if(value < 1) {
+                if (value < 1) {
 
                 } else {
                     setUser(value)
                 }
                 break;
             case 'email':
-                if(value < 1) {
-                    
+                if (value < 1) {
+
                 } else {
                     setEmail(value)
                 }
                 break;
             case 'estado':
-                if(value < 1) {
+                if (value < 1) {
 
                 } else {
                     setEstado(value)
                 }
                 break;
             case 'id':
-                if(value < 1) {
+                if (value < 1) {
 
                 } else {
                     setId(value)
                 }
                 break;
             case 'nombre':
-                if(value < 1) {
+                if (value < 1) {
 
                 } else {
                     setNombre(value)
@@ -82,8 +82,8 @@ const CreateSecretary = () => {
 
     function handleSubmit(params) {
         let account = { celular, password, email, estado, id, nombre, usuario }
-        
-        Firebase.write("Secretario",account)
+
+        Firebase.write("Secretario", account)
 
         if (account) {
             console.log('account:', account)
@@ -92,93 +92,108 @@ const CreateSecretary = () => {
 
     return (
         <Segment color="teal" className='CreateSecretary-container'>
-            <Header.Subheader>Nombre Completo</Header.Subheader>
-            <Input
-                focus
-                icon = "user"
-                id='nombre'
-                name='nombre'
-                placeholder='Ingrese su nombre'
-                type='text'
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-                className='input-error'
-            />
-            
-            <Header.Subheader>Cedula</Header.Subheader>
-            <Input 
-                focus
-                icon = "id card"
-                id='cedula'
-                name='id'
-                placeholder='Ingrese su identificación'
-                type='text'
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-                className='regular-style'
-            />
+            <Grid centered columns={2} container>
+                <Grid.Row centered>
+                    <Grid.Column>
+                        <center>
 
-            <Header.Subheader>Telefono</Header.Subheader>
-            <Input 
-                focus
-                icon = "phone"
-                id='telefono'
-                name='celular'
-                placeholder='Ingrese su numero celular'
-                type='text'
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-                className='regular-style'
+                            Nombre Completo
+                        <Input
+                                fluid
+                                focus
+                                icon="user"
+                                id='nombre'
+                                name='nombre'
+                                placeholder='Ingrese su nombre'
+                                type='text'
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                                className='input-error'
+                            />
 
-            />
-            
-            <Header.Subheader>Correo</Header.Subheader>
-            <Input 
-                focus
-                icon="envelope"
-                id='correo'
-                name='email'
-                placeholder='Ingrese su correo'
-                type='text'
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-                className='regular-style'
-            />
+                            Cedula
+                            <Input
+                                fluid
+                                focus
+                                icon="id card"
+                                id='cedula'
+                                name='id'
+                                placeholder='Ingrese su identificación'
+                                type='text'
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                                className='regular-style'
+                            />
 
-            <Header.Subheader>Nombre de usuario</Header.Subheader>
-            <Input
-                focus
-                icon = "user circle"
-                id='usuario'
-                name='usuario'
-                placeholder='Ingresa username'
-                type='text'
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-                className='input-error'
-            />
-            
-            <Header.Subheader>Contraseña</Header.Subheader>
-            <Input
-                focus
-                icon="key"
-                id='contraseña'
-                name='password'
-                placeholder='Ingrese su contraseña'
-                type='password'
-                onChange={(e) => handleChange(e.target.name, e.target.value)}
-                className='input-error'
-            />
-            {
-                passwordError &&
-                <Label pointing="above" className='label-error'>
-                    contraseña invalida
-                </Label>
-            }
-            <hr></hr>
-            <div>
-            <button class="ui primary button" onClick={(e) => handleSubmit()}>
-                Registrarse
-             </button>
-             <button class="ui button" onClick={(e) => setCreateSecretary(false)}>
-                Cancelar
-             </button>
-            </div>
+                            <Header.Subheader>Telefono</Header.Subheader>
+                            <Input
+                                fluid
+                                focus
+                                icon="phone"
+                                id='telefono'
+                                name='celular'
+                                placeholder='Ingrese su numero celular'
+                                type='text'
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                                className='regular-style'
+
+                            />
+
+                            <Header.Subheader>Correo</Header.Subheader>
+                            <Input
+                                fluid
+                                focus
+                                icon="envelope"
+                                id='correo'
+                                name='email'
+                                placeholder='Ingrese su correo'
+                                type='text'
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                                className='regular-style'
+                            />
+
+                            <Header.Subheader>Nombre de usuario</Header.Subheader>
+                            <Input
+                                fluid
+                                focus
+                                icon="user circle"
+                                id='usuario'
+                                name='usuario'
+                                placeholder='Ingresa username'
+                                type='text'
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                                className='input-error'
+                            />
+
+                            <Header.Subheader>Contraseña</Header.Subheader>
+                            <Input
+                                fluid
+                                focus
+                                icon="key"
+                                id='contraseña'
+                                name='password'
+                                placeholder='Ingrese su contraseña'
+                                type='password'
+                                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                                className='input-error'
+                            />
+                            {
+                                passwordError &&
+                                <Label pointing="above" className='label-error'>
+                                    Contraseña invalida
+                                </Label>
+                            }
+                            <br></br>
+                            <Button.Group fluid>
+                                <Button primary onClick={(e) => handleSubmit()}>
+                                    Registrarse
+                            </Button>
+                                <Button onClick={(e) => setCreateSecretary(false)}>
+                                    Cancelar
+                            </Button>
+                            </Button.Group>
+                        </center>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         </Segment>
     )
 };
